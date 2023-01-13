6b7eb3b716da4fbb89493c89d370ad3ab4f61e68
jaya193 <87478278+jaya193@users.noreply.github.com>
2023-01-13 09:34:14 +0530

    Add files via upload
    Odd one in Array -non duplicate 4/20

---
+++ day2/OddOneInArray2.java
@@ -0,0 +1,33 @@
+package week1.day2;
+
+import java.util.Arrays;
+
+public class OddOneInArray2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] nums = {2,2,1}; 
+		Arrays.sort(nums);
+		//System.out.println(nums);
+		for(int i=0;i<nums.length;i++)
+		{   int j;
+			for(j=0;j<nums.length;j++)
+			{
+				if(nums[i]==nums[j]&& i!=j)
+				{
+					//System.out.println(nums[i]);
+					break;
+			}
+			}
+				if(j == nums.length)
+				{
+					System.out.println(nums[i]);
+				}	
+				
+			
+			
+		}
+    
+	}
+
+}
